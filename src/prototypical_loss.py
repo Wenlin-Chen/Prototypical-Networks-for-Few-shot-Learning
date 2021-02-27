86943677062160c80abf6b8ccf92c16381e128a4
Gabrielle M.Y. Lau <laumingyi@gmail.com>
2021-02-27 10:34:10 +0000

    minor style edits

--- src/prototypical_loss.py
+++ src/prototypical_loss.py
@@ -19,16 +19,19 @@ class PrototypicalLoss(torch.nn.Module):
 
     def prototypical_loss(self, input, target, n_support):
         """
-        Average support samples to get prototype center and compute one batch's
-        loss w.r.t Euclidean distance between query samples and center
-        input:      x-embedding of shape [batch_size, 64], where batch_size is
-                    (n_support + n_query) * n_class
+        Average support samples to get prototype center and compute batch's
+        loss w.r.t Euclidean distance between query samples and centers
+        input:      x-embedding of shape [batch_size, 64], 
+                    batch_size = (n_support + n_query) * n_class
         target:     class labels for each input point in the batch
         n_support:  number of examples per class
         """
+
         # Extract unique classes and initialise variables
-        # input_to_class_idx maps each input to the unique_class by index 0-59
-        unique_classes, input_to_class_idx = torch.unique(target, sorted=False, return_inverse=True)
+        # input_to_class_idx maps each input to one of unique_classes 
+        # by index 0 to n_class - 1 (instead of dataset's class numbering)
+        unique_classes, input_to_class_idx = torch.unique(target, sorted=False,
+                                                        return_inverse=True)
         n_class = len(unique_classes)
         query_list = []
         prototype_list = []
@@ -36,7 +39,8 @@ class PrototypicalLoss(torch.nn.Module):
         for k in range(n_class):
             # Get input indices corresponding to class k, flatten to vector
             # input_idx shape: [10]
-            input_idx = torch.flatten((input_to_class_idx == k).nonzero(as_tuple=False))
+            input_idx = torch.flatten((input_to_class_idx == k).nonzero(
+                                                        as_tuple = False))
             
             # Extract inputs corresponding to class k
             # input_k shape: [n_class, 64]
@@ -56,24 +60,28 @@ class PrototypicalLoss(torch.nn.Module):
             query_list.append(query_k)
             prototype_list.append(prototype_k)
 
-        # stack classes on top of each other
+        # Stack classes on top of each other
         # query shape: [n_class, n_query, 64]
         query = torch.stack(query_list)
         # prototype shape: [n_class, 64]
         prototype = torch.stack(prototype_list)
 
-        # query target with index from 0 to n_class-1 
+        # Make query target with index from 0 to n_class-1 
         # query_target shape (matches log_p_y): [n_class, n_class, n_query]
         # query_target2 shape (matches y_hat): [n_class, n_query]
         n_query = query_list[0].size()[0]
-        query_target = torch.arange(0, n_class).view(n_class, 1, 1).expand(n_class, n_class, n_query)
-        query_target2 = torch.arange(0, n_class).view(n_class, 1).expand(n_class, 5)
+        query_target = torch.arange(0, n_class).view(n_class, 1, 1).expand(
+                                                    n_class, n_class, n_query)
+        query_target2 = torch.arange(0, n_class).view(n_class, 1).expand(
+                                                                n_class, 5)
 
-        # Compute distances between each class' queries and each class prototype 
+        # Compute distances between each class' queries and each class' 
+        # prototype 
         # dists shape: [n_class, n_class, n_query] 
         dists = self.euclidean_dist(prototype, query)
 
-        # Pass negative distances through softmax to give log likelihood log(p(y=k|x))
+        # Pass negative distances through softmax to give log likelihood 
+        # log(p(y=k|x))
         # log_p_y shape: [n_class, n_class, n_query] 
         log_p_y = torch.nn.functional.softmax(-dists, dim=1)
 
@@ -81,7 +89,7 @@ class PrototypicalLoss(torch.nn.Module):
         # y_hat shape: [n_class, n_query]
         _, y_hat = log_p_y.max(dim=1)
 
-        print("""query: \t {} \n 
+        print("""query: \t \t {} \n 
             prototype: \t \t {} \n 
             dists: \t \t {} \n 
             query_target: \t {} \n 
@@ -97,7 +105,7 @@ class PrototypicalLoss(torch.nn.Module):
         # Accuracy is the average number of correct classification
         loss_val = -log_p_y.gather(1, query_target).mean()
         acc_val = y_hat.eq(query_target2).float().mean()
-        print("loss_val: {} \n acc_val: {}".format(loss_val, acc_val))
+        print("loss_val = {} \n acc_val = {}".format(loss_val, acc_val))
         print("---------------------------------------------------------")
 
         return loss_val, acc_val
