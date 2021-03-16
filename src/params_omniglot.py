c8967ae3640273e12214a028b1aba92aa32aae91
Wenlin Chen <chen.wenlin@outlook.com>
2021-03-16 20:57:45 +0000

    minor

--- src/params_omniglot.py
+++ src/params_omniglot.py
@@ -3,10 +3,10 @@ use_cuda = True
 seed = 1
 
 classes_per_it_tr = 60 # train way
-num_support_tr = 1 # shot = train shot = test shot
+num_support_tr = 5 # shot = train shot = test shot
 num_query_tr = 5
 
-classes_per_it_val = 20 # way
+classes_per_it_val = 5 # way
 num_support_val = num_support_tr # shot = test shot = train shot
 num_query_val = 15
 
