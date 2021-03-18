7b9694ae5f077b5a387608336fd887646d8d625b
Gabrielle M.Y. Lau <laumingyi@gmail.com>
2021-03-18 15:17:46 +0000

    Added params for CUB

---
+++ src/params_cub.py
@@ -0,0 +1,15 @@
+CUB_data_path = '../../CUB_data/CUB_200_2011' # Assume dataset downloaded here
+
+epochs = 100
+batch_size = 64
+num_workers = 4
+
+n_class = 200
+n_class_train = 100
+n_class_val = 50
+
+samples_per_class = 29
+num_query_tr = 10
+
+seed = 1
+use_cuda = True
